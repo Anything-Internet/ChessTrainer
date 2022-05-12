aaa289e1b31533aa28e9cc1b3cef909e4f1a5ca3
Jerry Hobby <Jerry@JerryHobby.com>
2022-05-11 19:34:29 -0500

    new pieces

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,22 @@ void main() {
 }
 
 class AppThemeData {
-  final BorderRadius borderRadius = BorderRadius.circular(8);
+  late double squareWidth;
+  late double squareHeight;
+  late Color darkSquare;
+  late Color lightSquare;
+  late Color themeColor;
 
-  Color darkSquare = Colors.amber;
-  Color lightSquare = Colors.white;
-
-  double squareWidth = 50;
-  double squareHeight = 50;
+  AppThemeData() {
+    squareHeight = squareWidth = 45;
+    themeColor = Colors.deepPurple;
+    darkSquare = const Color.fromRGBO(0x99, 0x99, 0xcc, 100);
+    lightSquare = const Color.fromRGBO(0xee, 0xee, 0xff, 100);
+  }
 
   ThemeData get materialTheme {
     return ThemeData(
-      primaryColor: Colors.deepPurple,
+      primaryColor: themeColor,
     );
   }
 }
@@ -27,20 +32,33 @@ Column drawChessBoard(BoardData boardData) {
   List<Row> board = [];
   List<Container> square = [];
 
-  AssetImage whiteKingImg = const AssetImage('assets/set2_1x/images/whiteKing.png');
-  AssetImage whiteQueenImg = const AssetImage('assets/set2_1x/images/whiteQueen.png');
-  AssetImage whiteBishopImg = const AssetImage('assets/set2_1x/images/whiteBishop.png');
-  AssetImage whiteKnightImg = const AssetImage('assets/set2_1x/images/whiteKnight.png');
-  AssetImage whiteRookImg = const AssetImage('assets/set2_1x/images/whiteRook.png');
-  AssetImage whitePawnImg = const AssetImage('assets/set2_1x/images/whitePawn.png');
-  AssetImage blackKingImg = const AssetImage('assets/set2_1x/images/blackKing.png');
-  AssetImage blackQueenImg = const AssetImage('assets/set2_1x/images/blackQueen.png');
-  AssetImage blackBishopImg = const AssetImage('assets/set2_1x/images/blackBishop.png');
-  AssetImage blackKnightImg = const AssetImage('assets/set2_1x/images/blackKnight.png');
-  AssetImage blackRookImg = const AssetImage('assets/set2_1x/images/blackRook.png');
-  AssetImage blackPawnImg = const AssetImage('assets/set2_1x/images/blackPawn.png');
+  AssetImage whiteKingImg =
+      const AssetImage('assets/set2_1x/images/whiteKing.png');
+  AssetImage whiteQueenImg =
+      const AssetImage('assets/set2_1x/images/whiteQueen.png');
+  AssetImage whiteBishopImg =
+      const AssetImage('assets/set2_1x/images/whiteBishop.png');
+  AssetImage whiteKnightImg =
+      const AssetImage('assets/set2_1x/images/whiteKnight.png');
+  AssetImage whiteRookImg =
+      const AssetImage('assets/set2_1x/images/whiteRook.png');
+  AssetImage whitePawnImg =
+      const AssetImage('assets/set2_1x/images/whitePawn.png');
+  AssetImage blackKingImg =
+      const AssetImage('assets/set2_1x/images/blackKing.png');
+  AssetImage blackQueenImg =
+      const AssetImage('assets/set2_1x/images/blackQueen.png');
+  AssetImage blackBishopImg =
+      const AssetImage('assets/set2_1x/images/blackBishop.png');
+  AssetImage blackKnightImg =
+      const AssetImage('assets/set2_1x/images/blackKnight.png');
+  AssetImage blackRookImg =
+      const AssetImage('assets/set2_1x/images/blackRook.png');
+  AssetImage blackPawnImg =
+      const AssetImage('assets/set2_1x/images/blackPawn.png');
   AssetImage emptyImg = const AssetImage('assets/empty.png');
-  AssetImage woodTileImg = const AssetImage('assets/set2_1x/images/woodTile.png');
+  AssetImage woodTileImg =
+      const AssetImage('assets/set2_1x/images/woodTile.png');
 
   int colA = "A".codeUnitAt(0);
   int colH = "H".codeUnitAt(0);
@@ -78,41 +96,45 @@ Column drawChessBoard(BoardData boardData) {
 
       square.add(
         Container(
-          alignment: Alignment.bottomCenter,
-          height: appThemeData.squareHeight,
-          width: appThemeData.squareWidth,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: woodTileImg,
-              opacity: 0.10,
-              fit: BoxFit.cover,
-            ),
+          width: appThemeData.squareHeight,
+          child: AspectRatio(
+            aspectRatio: 1.0,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: woodTileImg,
+                  opacity: 0.10,
+                  fit: BoxFit.cover,
+                ),
 
-            //backgroundBlendMode: BlendMode.hardLight,
-             gradient: RadialGradient(
-               radius: 0.9,
-               colors: [
-                 color.withOpacity(0.3),
-                 color,
-               ],
-             ),
-          ),
-          child: Draggable(
-            child: Image(
-              image: thisPiece,
-              height: appThemeData.squareHeight,
-              width: appThemeData.squareWidth,
+                //backgroundBlendMode: BlendMode.hardLight,
+                gradient: RadialGradient(
+                  radius: 0.9,
+                  colors: [
+                    color.withOpacity(0.3),
+                    color,
+                  ],
+                ),
+              ),
+              child: Draggable(
+                child: Image(
+                  image: thisPiece,
+                  height: appThemeData.squareHeight,
+                  width: appThemeData.squareWidth,
+                ),
+                feedback: Image(
+                  image: thisPiece,
+                  height: appThemeData.squareHeight * 1.25,
+                  width: appThemeData.squareWidth * 1.25,
+                ),
+                data: [
+                  contents,
+                  row,
+                  col,
+                ],
+              ),
             ),
-            feedback: Image(
-              image: thisPiece,
-              height: appThemeData.squareHeight * 1.5,
-              width: appThemeData.squareWidth * 1.5,
-            ),
-            data: [
-              contents,
-              row,
-              col,
-            ],
           ),
         ),
       );
@@ -149,7 +171,7 @@ class _MyAppState extends State<MyApp> {
   String puzzleFile = "assets/puzzles.csv";
 
   _MyAppState() {
-    DesktopWindow.setWindowSize(const Size(700, 1000));
+    DesktopWindow.setWindowSize(const Size(700, 1200));
 
     boardData.SetPosition(boardStartPosition);
     chessPuzzles.loadPuzzles(puzzleFile);
@@ -168,12 +190,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    appThemeData.darkSquare = const Color.fromRGBO(0x99, 0x99, 0xcc, 100);
-    appThemeData.lightSquare = const Color.fromRGBO(0xee, 0xee, 0xff, 100);
-    appThemeData.squareHeight = 40;
-    appThemeData.squareWidth = 40;
     Color turnColor = Colors.white;
-
     if (boardData.playersTurn == "b") turnColor = Colors.black;
 
     return MaterialApp(
@@ -213,6 +230,11 @@ class _MyAppState extends State<MyApp> {
                   ],
                 ),
               ),
+              Row(children: [
+                Container(
+                  height: 10,
+                ),
+              ]),
               drawChessBoard(boardData),
             ],
           ),
@@ -231,18 +253,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-// Future testWindowFunctions() async {
-//   Size size = await DesktopWindow.getWindowSize();
-//   print(size);
-//   await DesktopWindow.setWindowSize(Size(700,1000));
-//
-//   await DesktopWindow.setMinWindowSize(Size(700,800));
-//   await DesktopWindow.setMaxWindowSize(Size(800,800));
-//
-//   await DesktopWindow.resetMaxWindowSize();
-//   await DesktopWindow.toggleFullScreen();
-//   bool isFullScreen = await DesktopWindow.getFullScreen();
-//   await DesktopWindow.setFullScreen(true);
-//   await DesktopWindow.setFullScreen(false);
-// }
